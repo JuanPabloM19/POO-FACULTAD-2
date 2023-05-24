338f7ebecd91a9204d97ec7823194608ea54acea
Juan Pablo <jpmunizaga19@gmail.com>
2023-05-24 11:29:37 -0300

    for x while

--- Ejercicio4/claseColeccion.py
+++ Ejercicio4/claseColeccion.py
@@ -4,7 +4,6 @@ from claseEmpleado_planta import EmpleadoPlanta
 from claseEmpleado_contratado import EmpleadoContratado
 from claseEmpleado_externo import EmpleadoExterno
 
-
 class ColeccionEmpleados:
     def __init__(self, max_empleados):
         self.__empleados = []
@@ -16,25 +15,6 @@ class ColeccionEmpleados:
         else:
             self.__empleados.append(empleado)
 
-    def listar_empleados(self):
-        for empleado in self.__empleados:
-            if empleado is not None:
-                print(f"DNI: {empleado.get_dni()}, Nombre: {empleado.get_nombre()}, Sueldo: {empleado.calcular_sueldo()}")
-
-    def calcular_sueldo_por_tipo(self, tipo_empleado):
-        sueldo_total = 0
-        for empleado in self.__empleados:
-            if isinstance(empleado, tipo_empleado):
-                sueldo_total += empleado.calcular_sueldo()
-        return sueldo_total
-
-    def obtener_empleado_con_mayor_sueldo(self):
-        empleado_mayor_sueldo = None
-        for empleado in self.__empleados:
-            if empleado_mayor_sueldo is None or empleado.calcular_sueldo() > empleado_mayor_sueldo.calcular_sueldo():
-                empleado_mayor_sueldo = empleado
-        return empleado_mayor_sueldo
-
     def registrar_horas(self, dni, horas_trabajadas):
         empleado = self.buscar_empleado(dni)
         if empleado is not None:
@@ -45,7 +25,6 @@ class ColeccionEmpleados:
         else:
             print(f"No se encontró ningún empleado con DNI {dni}.")
 
-
     def total_tarea(self, tarea):
         total = 0
         for empleado in self.__empleados:
@@ -53,19 +32,56 @@ class ColeccionEmpleados:
                 total += empleado.calcular_pago()
         print(f"El monto a pagar por la tarea {tarea} es de ${total}.")
 
+    def listar_empleados(self):
+        i = 0
+        while i < len(self.__empleados):
+            empleado = self.__empleados[i]
+            if empleado is not None:
+                print(f"DNI: {empleado.get_dni()}, Nombre: {empleado.get_nombre()}, Sueldo: {empleado.calcular_sueldo()}")
+            i += 1
+
+    def calcular_sueldo_por_tipo(self, tipo_empleado):
+        sueldo_total = 0
+        i = 0
+        while i < len(self.__empleados):
+            empleado = self.__empleados[i]
+            if isinstance(empleado, tipo_empleado):
+                sueldo_total += empleado.calcular_sueldo()
+            i += 1
+        return sueldo_total
+
+    def obtener_empleado_con_mayor_sueldo(self):
+        empleado_mayor_sueldo = None
+        i = 0
+        while i < len(self.__empleados):
+            empleado = self.__empleados[i]
+            if empleado_mayor_sueldo is None or empleado.calcular_sueldo() > empleado_mayor_sueldo.calcular_sueldo():
+                empleado_mayor_sueldo = empleado
+            i += 1
+        return empleado_mayor_sueldo
+
     def ayuda_economica(self):
         print("Empleados que les corresponde ayuda económica:")
-        for empleado in self.__empleados:
+        i = 0
+        while i < len(self.__empleados):
+            empleado = self.__empleados[i]
             if empleado.calcular_sueldo() < 150000:
                 print(f"Nombre: {empleado.get_nombre()}, Dirección: {empleado.get_direccion()}, DNI: {empleado.get_dni()}")
+            i += 1
 
     def calcular_sueldo(self):
         print("Sueldo de todos los empleados:")
-        for empleado in self.__empleados:
+        i = 0
+        while i < len(self.__empleados):
+            empleado = self.__empleados[i]
             print(f"Nombre: {empleado.get_nombre()}, Teléfono: {empleado.get_telefono()}, Sueldo: {empleado.calcular_sueldo()}")
+            i += 1
 
     def buscar_empleado(self, dni):
-        for empleado in self.__empleados:
+        i = 0
+        while i < len(self.__empleados):
+            empleado = self.__empleados[i]
             if empleado.get_dni() == dni:
                 return empleado
+            i += 1
         return None
