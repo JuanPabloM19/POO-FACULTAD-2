81a8beb4d906107b8e02bd50faa11ab828ed8bf0
Juan Pablo <jpmunizaga19@gmail.com>
2023-05-29 09:08:34 -0300

    for por while

--- Ejercicio7/claseLista.py
+++ Ejercicio7/claseLista.py
@@ -14,24 +14,45 @@ class Lista:
     def agregar_agente_a_posicion(self, posicion: int, agente: Agente):
         self.__agentes.insert(posicion, agente)
 
-    def mostrar_agente_por_posicion(self, posicion: int) -> Agente:
-        return self.__agentes[posicion]
+    def mostrar_agente_por_posicion(self, posicion):
+        i = 0
+        while i < len(self.__agentes):
+            if i == posicion:
+                return self.__agentes[i]
+            i += 1
 
-    def generar_listado_docentes_investigadores_por_area(self, area_investigacion: str) -> List[DocenteInvestigador]:
-        return [agente for agente in self.__agentes if isinstance(agente, DocenteInvestigador) and agente.__área_investigación == area_investigacion]
+    def generar_listado_docentes_investigadores_por_area(self, area_investigacion):
+        i = 0
+        listado = []
+        while i < len(self.__agentes):
+            agente = self.__agentes[i]
+            if isinstance(agente, DocenteInvestigador) and agente.get_area_investigacion() == area_investigacion:
+                listado.append(agente)
+            i += 1
+        return listado
 
     def generar_listado_agentes_por_categoria(self, categoria_investigacion):
-        return [agente for agente in self.__agentes if isinstance(agente, DocenteInvestigador) and agente.get_categoria_programa() == categoria_investigacion]
+        i = 0
+        listado = []
+        while i < len(self.__agentes):
+            agente = self.__agentes[i]
+            if isinstance(agente, DocenteInvestigador) and agente.get_categoria_programa() == categoria_investigacion:
+                listado.append(agente)
+            i += 1
+        return listado
 
     def recorrer_lista_e_generar_listado(self):
+        i = 0
         listado = []
-        for agente in self.__agentes:
+        while i < len(self.__agentes):
+            agente = self.__agentes[i]
             sueldo = agente.get_sueldo_basico() + agente.get_antiguedad() * 0.1
             if isinstance(agente, DocenteInvestigador):
                 sueldo += agente.get_importe_extra()     
             cargo = agente.get_cargo()
             catedra = agente.get_catedra()
             listado.append((agente.get_nombre(), agente.get_apellido(), cargo, catedra, sueldo))
+            i += 1
         return listado
 
     def almacenar_agentes_en_archivo(self):
